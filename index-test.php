1a2e2b075e59289cfefebe6c193622a01d55bb36
kaglim <kaglim@gmail.com>
2010-08-07 14:48:03 +0800

    test commit changes

--- index-test.php
+++ index-test.php
@@ -2,6 +2,7 @@
 /**
  * This is the bootstrap file for test application.
  * This file should be removed when the application is deployed for production.
+****test
  */
 
 // change the following paths if necessary
