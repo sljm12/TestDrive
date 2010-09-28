4e1ac305507e160f298cbfbb397d752a574c371d
unknown <stephen@.(none)>
2010-09-28 22:18:15 +0800

    Added javascript to populate the openid identifier when clicked on a provider

--- protected/views/site/openIDLogin.php
+++ protected/views/site/openIDLogin.php
@@ -1,6 +1,11 @@
+
 <div class="form">
 <?php echo CHtml::beginForm(); ?>
  
+	<div class="providers">
+		<span id="google">Google</span>
+		<span id="yahoo">Yahoo</span>
+	</div>
     <div class="row">
         <?php echo CHtml::label('Identifier:', 'openid_identifier'); ?>
         <?php echo CHtml::textField('openid_identifier', '', array('size'=>40)); ?>
@@ -15,3 +20,37 @@
  
 <?php echo CHtml::endForm(); ?>
 </div><!-- form -->
+<script>
+	var providers=[];
+	providers["google"]="https://www.google.com/accounts/o8/id";
+	providers["yahoo"]="https://me.yahoo.com";
+	
+	function setOpenIdIdentifier(url){
+		$('#openid_identifier').val(url);
+	}
+	
+	function addClickHandler(){
+		for(key in providers){			
+			var selector='#'+key;
+			alert(selector);
+			var url=providers[key];
+			$(selector).click(function(){
+				setOpenIdIdentifier(url);
+			});
+		}
+	}
+	
+	//addClickHandler();
+	
+	$('#google').click(function(handler){		
+		setOpenIdIdentifier(providers['google']);
+		
+	});
+	
+	$('#yahoo').click(function(handler){		
+		setOpenIdIdentifier(providers['yahoo']);
+		
+	});
+	
+	
+</script>
