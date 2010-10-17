2d3d12cb87d797a18349aff38aced6c6921afeec
sljm12 <leejianm@hotmail.com>
2010-10-18 06:23:12 +0800

    Added google analytics code

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -3,7 +3,19 @@
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="language" content="en" />
+	<script type="text/javascript">
 
+		var _gaq = _gaq || [];
+		_gaq.push(['_setAccount', 'UA-19101875-1']);
+		_gaq.push(['_trackPageview']);
+
+		(function() {
+		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		})();
+
+	</script>
 	<script src="<?php echo Yii::app()->request->baseUrl; ?>/js/jquery.js" type="text/javascript"></script>
 	
 	<!-- blueprint CSS framework -->
@@ -19,6 +31,8 @@
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/content.css" />
 	
 	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
+	
+	
 </head>
 
 <body>
