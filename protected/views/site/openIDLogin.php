6a253835e2fbe56cc8f19fff58e66ce1a6f053b3
sljm12 <leejianm@hotmail.com>
2010-11-18 23:34:51 +0800

    Added facebook and myopenid pictuers to the login
    Edited the google and yahoo png files to make sure all the providers look about the same
    in terms of width and height of the images
    
    Facebook login is not implemented yet.

--- protected/views/site/openIDLogin.php
+++ protected/views/site/openIDLogin.php
@@ -1,18 +1,29 @@
-
+<style>
+.providers{
+	vertical-align:middle;
+}
+</style>
 <div class="form">
 <?php echo CHtml::beginForm(); ?>
-	Click on your OpenID provider below<p>
+	If you have a Google, Yahoo, any OpenId provider, you can login directly to ipostishop, there is no need to sign up !<p>
+	<p>
+	What are the benefits?
+	<ol>
+		<li>There is no need to remember another username/password!</li>
+		<li>Faster signup process.</li>
+		<li>Focus of the below providers are identiy management, they are more through about protecting your identity.</li>
+	</ol>
+		
 	<div class="providers">
-		<span id="google" class="provider"><img src="<?php echo Yii::app()->request->baseUrl;?>/assets/openid/google.jpg" height=70/></span>
-		<span id="yahoo" class="provider"><img src="<?php echo Yii::app()->request->baseUrl;?>/assets/openid/yahoo.jpg" height=70/></span>
+		<span id="google" class="provider"><img src="<?php echo Yii::app()->request->baseUrl;?>/assets/openid/google.png" width=100/></span>
+		<span id="yahoo" class="provider"><img src="<?php echo Yii::app()->request->baseUrl;?>/assets/openid/yahoo.png" width=100/></span>
+		<span id="facebook" class="provider"><img src="<?php echo Yii::app()->request->baseUrl;?>/assets/openid/facebook.jpg" width=100/></span>
+		<span id="myopenid" class="provider"><img src="<?php echo Yii::app()->request->baseUrl;?>/assets/openid/myopenid.jpg" width=100/></span>
 	</div>
 	<p>
     <div class="row">
         <?php echo CHtml::label('Identifier:', 'openid_identifier'); ?>
-        <?php echo CHtml::textField('openid_identifier', '', array('size'=>40)); ?>
-        <p class="hint">
-            Hint: You may login with <tt>https://www.google.com/accounts/o8/id</tt>.
-        </p>
+        <?php echo CHtml::textField('openid_identifier', '', array('size'=>40)); ?>        
     </div>
  
     <div class="row buttons">
@@ -25,6 +36,7 @@
 	var providers=[];
 	providers["google"]="https://www.google.com/accounts/o8/id";
 	providers["yahoo"]="https://me.yahoo.com";
+	providers["myopenid"]="http://myopenid.com";
 	
 	function setOpenIdIdentifier(url){
 		$('#openid_identifier').val(url);
@@ -51,6 +63,10 @@
 	$('#yahoo').click(function(handler){		
 		setOpenIdIdentifier(providers['yahoo']);
 		
+	});
+	
+	$('#myopenid').click(function(handler){
+		setOpenIdIdentifier(providers['myopenid']);
 	});
 	
 	
