ae481897f9b8fe77dfb9421db7716c5580f37a56
sljm12 <leejianm@hotmail.com>
2011-05-20 23:00:14 +0800

    Regenerated the Post.php for the added columns added in the schema

--- protected/models/Post.php
+++ protected/models/Post.php
@@ -2,20 +2,25 @@
 
 /**
  * This is the model class for table "post".
+ *
+ * The followings are the available columns in table 'post':
+ * @property integer $id
+ * @property string $title
+ * @property string $url
+ * @property string $remarks
+ * @property integer $clicks
+ * @property string $dateUpdated
+ * @property string $userid
+ * @property string $file_hash
+ * @property integer $blogid
+ *
+ * The followings are the available model relations:
+ * @property Blogshop $blog
+ * @property Userdetails $user
+ * @property Category[] $categories
  */
 class Post extends CActiveRecord
 {
-	/**
-	 * The followings are the available columns in table 'post':
-	 * @var integer $id
-	 * @var string $title
-	 * @var string $url
-	 * @var string $remarks
-	 * @var integer $clicks
-	 * @var string $dateUpdated
-	 * @var string $userid
-	 */
-
 	/**
 	 * Returns the static model of the specified AR class.
 	 * @return Post the static model class
@@ -41,15 +46,15 @@ class Post extends CActiveRecord
 		// NOTE: you should only define rules for those attributes that
 		// will receive user inputs.
 		return array(
-			array('title, url, remarks', 'required'),
-			array('clicks', 'numerical', 'integerOnly'=>true),
+			array('title, url, remarks, dateUpdated', 'required'),
+			array('clicks, blogid', 'numerical', 'integerOnly'=>true),
 			array('title, url', 'length', 'max'=>255),
 			array('remarks', 'length', 'max'=>500),
 			array('userid', 'length', 'max'=>300),
-			array('dateUpdated', 'safe'),
+			array('file_hash', 'length', 'max'=>64),
 			// The following rule is used by search().
 			// Please remove those attributes that should not be searched.
-			array('id, title, url, remarks, clicks, dateUpdated, userid', 'safe', 'on'=>'search'),
+			array('id, title, url, remarks, clicks, dateUpdated, userid, file_hash, blogid', 'safe', 'on'=>'search'),
 		);
 	}
 
@@ -61,8 +66,9 @@ class Post extends CActiveRecord
 		// NOTE: you may need to adjust the relation name and the related
 		// class name for the relations automatically generated below.
 		return array(
-			'categories' => array(self::MANY_MANY, 'Category', 'post_category(postid, categoryid)'),
+			'blog' => array(self::BELONGS_TO, 'Blogshop', 'blogid'),
 			'user' => array(self::BELONGS_TO, 'Userdetails', 'userid'),
+			'categories' => array(self::MANY_MANY, 'Category', 'post_category(postid, categoryid)'),
 		);
 	}
 
@@ -79,6 +85,8 @@ class Post extends CActiveRecord
 			'clicks' => 'Clicks',
 			'dateUpdated' => 'Date Updated',
 			'userid' => 'Userid',
+			'file_hash' => 'File Hash',
+			'blogid' => 'Blogid',
 		);
 	}
 
@@ -94,18 +102,14 @@ class Post extends CActiveRecord
 		$criteria=new CDbCriteria;
 
 		$criteria->compare('id',$this->id);
-
 		$criteria->compare('title',$this->title,true);
-
 		$criteria->compare('url',$this->url,true);
-
 		$criteria->compare('remarks',$this->remarks,true);
-
 		$criteria->compare('clicks',$this->clicks);
-
 		$criteria->compare('dateUpdated',$this->dateUpdated,true);
-
 		$criteria->compare('userid',$this->userid,true);
+		$criteria->compare('file_hash',$this->file_hash,true);
+		$criteria->compare('blogid',$this->blogid);
 
 		return new CActiveDataProvider(get_class($this), array(
 			'criteria'=>$criteria,
@@ -118,7 +122,7 @@ class Post extends CActiveRecord
 		$command->bindParam(':postid',$postid,PDO::PARAM_INT);
 
 		foreach($categories as $category){
-			$command->bindParam(':categoryid',$category->id,PDO::PARAM_INT);
+			$command->bindValue(':categoryid',$category->id,PDO::PARAM_INT);
 			$command->execute();
 		}
 	}
